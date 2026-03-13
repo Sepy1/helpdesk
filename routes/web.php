b4cf40564b7295ddb38edb635c52422a3716c3ac
erik <sepy1234@gmail.com>
2026-03-13 14:10:13 +0700

    Revert "fixing preview lampiran mobile"
    This reverts commit 12c3abdf6012bd9b2dbe79eb99468b1cf47375fb.

--- routes/web.php
+++ routes/web.php
@@ -99,7 +99,6 @@ Route::middleware(['auth'])->group(function () {
     // ===== Shared: detail, komentar, lampiran =====
     
 Route::get('/ticket/{ticket}', [TicketController::class, 'show'])->name('ticket.show');
-Route::get('/ticket/{ticket}/view', [TicketController::class, 'viewAttachment'])->name('ticket.view');
 Route::get('/ticket/{ticket}/download', [TicketController::class, 'downloadAttachment'])->name('ticket.download');
 
 // Komentar tiket
@@ -110,9 +109,6 @@ Route::delete('/comment/{comment}', [TicketController::class, 'deleteComment'])
 
 Route::get('/ticket/comment/{comment}/download', [TicketController::class, 'downloadCommentAttachment'])
     ->name('comment.download');
-
-Route::get('/ticket/comment/{comment}/view', [TicketController::class, 'viewCommentAttachment'])
-    ->name('comment.view');
 
     
     // ===== CABANG =====
